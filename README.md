7f2f27f8efecd7728f1dcda649a58bac8fdb92e0
AJ Schiller <abigailschiller@gmail.com>
2021-01-29 17:17:49 +0100

    Converted several numpy-based operations to pandas, removed `smooth` function, refined plotting/mapping functions. Added README.md.

---
+++ README.md
@@ -0,0 +1,92 @@
+# Climate Modelling at VU Amsterdam
+
+[![](https://img.shields.io/badge/python-3.7-blue.svg)](https://www.python.org/downloads/)
+
+## Description
+
+This module is intended for use in the graduate course Climate Modelling at VU Amsterdam. It was created based on the practical work assigned during the January 2021 fully-online presentation of the course. Mileage in future presentations may vary, and relevant contributions are of course welcome.
+
+## Getting started
+
+### Dependencies
+
+This module requires that the following packages also be installed:
+
+* [numpy](https://numpy.org/install/)
+* [pandas](https://pandas.pydata.org/docs/getting_started/install.html)
+* [matplotlib](https://matplotlib.org/users/installing.html)
+* [netCDF4](https://pypi.org/project/netCDF4/)
+* [cartopy](https://scitools.org.uk/cartopy/docs/latest/installing.html#installing)
+
+If you are using Anaconda, you will already have some of these packages installed. Use Anaconda to install any missing packages.<br>
+Otherwise, all packages are also available from PyPi. If you are unsure how to install a specific package, follow the respective link from the list above.
+
+### Installation
+
+<p align="right">
+<img style="float: right;" src="https://docs.github.com/assets/images/help/repository/code-button.png" alt="The 'Code' button (source: GitHub)" width="250"/>
+</p>
+
+To install this module, you can either click the green 'Code' button on the right of the [GitHub page](https://github.com/AJSchiller/climate_modelling_vu) and then click 'Download ZIP':
+
+<br><br>Or run the following command from your Terminal (linux/MacOS) or Command Prompt (Windows):
+
+`git clone https://github.com/AJSchiller/climate_modelling_vu.git`
+
+Before you can import this package into your python code, you will have to do one of the following:
+
+1. Place the file `climate_modelling.py` in the same folder as the rest of your python packages;
+2. Add the folder containing `climate_modelling.py` to your system's `PATH` variable (search the internet for instructions for your specific operating system); or
+3. Place the file `climate_modelling.py` in the same folder as the python script to which you want to import it (this is undesireable, as you would need to do this for every script in which you want to import the module).
+
+You should now be able to import the `climate_modelling` module into your python script using:
+
+``` python
+import climate_modelling as cm
+```
+
+If, for some reason, none of these options works, you can add the following to the start of your script (again, you would need to do this in every script that uses the module):
+
+``` python
+import sys
+sys.path.append(`path/to/the/folder/containing/the/module`)
+import climate_modelling as cm
+```
+
+## How to use this module
+
+This module is intended to help students quickly visualise selected model outputs for the practicals in the course Climate Modelling, allowing you to go through large amounts of information relatively quickly. However, for the final report on the practicals, you will most likely want to use your own code to produce the necessary figures with appropriate annotation - you can of course adapt code contained in this module, if you wish.
+
+I will add an example script as soon as I have clearance to include the associated data files, and more detailed documentation will eventually follow. For now though, read on for a brief description of what this module can do, and refer to the comments in the `.py` file itself for further details.
+
+The module currently contains functions for handling two types of data:
+
+### 1. Spatial data contained in a netCDF file
+
+Start with the function `cm.list_vars`, which will simply print a list of the variables contained in the dataset, together with their full names, so that you can get a quick overview of the data you have.
+
+Once you've found the name of a variable you want to explore further, try the following to extract the relevant data:
+
+``` python
+var, lons, lats = cm.get_var(dataset, 'variable_name')
+```
+
+You now have arrays containing the variable values, as well as the corresponding longitudes and latitudes (you will need these later to map the data).<br>
+Note that there is no need to declare (create) new longitude and latitude variables every time you extract another variable from a dataset. If you already have these variables for the current dataset, you can get just the variable of interest using something like:
+
+```python
+var = cm.get_var(dataset, 'variable_name')[0]
+```
+
+Note that these variables usually have at least two dimensions, often more. Before you can produce a map of the data, you will have to reduce them to two dimensions - latitude and longitude only. Depending on what information you want, you might select an individual year of variable data or get an average over several years, to name two examples.<br>
+Once you have two-dimensional (lat-lon) data, you can quickly draw a map of it using either `cm.map_data` (draws a rectangular map of the whole world) or `cm.plot_nearside` (draws a globe, centred on a user-specified coordinate).
+
+### 2. Time series data
+
+The main model used in the course is iLOVECLIM [[1]](#1). One of its outputs is a time series of daily data in a file called something like `book*****` (with no file extension). The function `cm.read_book` reads this file and returns a time series of the annual mean of the data. You can use `cm.plot_timeline` to graph the annual mean together with a 10-year running mean, to see how the variable changes over the model run.
+
+
+## References
+
+<a name="1"></a>
+[1] Goosse, H., Brovkin, V., Fichefet, T., Haarsma, R., Huybrechts, P., Jongma, J., Mouchet, A., Selten, F., Barriat, P.-Y., Campin, J.-M., Deleersnijder, E., Driesschaert, E., Goelzer, H., Janssens, I., Loutre, M.-F., Morales Maqueda, M.A., Opsteegh, T., Mathieu, P.-P., Munhoven, G., Pettersson, E.J., Renssen, H., Roche, D.M., Schaeffer, M., Tartinville, B., Timmermann, A., Weber, S.L., 2010. Description of the Earth system model of intermediate complexity LOVECLIM version 1.2. Geoscientific Model Development 3, 603–633. [DOI: 10.5194/gmd-3-603-2010](https://doi.org/10.5194/gmd-3-603-2010)
--- climate_modelling.py
+++ climate_modelling.py
@@ -2,7 +2,7 @@
 
 """
 Created: 9 January 2021
-Last modified: 14 January 2021
+Last modified: 28 January 2021
 
 This module contains functions that are likely to be used repeatedly in computer practicals in the course Climate Modelling at VU Amsterdam, Jan 2021.
 
@@ -11,11 +11,12 @@ This module contains functions that are likely to be used repeatedly in computer
 
 
 import numpy as np
-from numpy import ma
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import pandas as pd
+from cartopy.util import add_cyclic_point
+from matplotlib.colors import TwoSlopeNorm
 
 
 ############################################################
@@ -52,7 +53,7 @@ def get_var(data, variable):
     """
 
     var = data.variables[variable]
-    var_plot =  ma.masked_equal(var, var.missing_value)
+    var_plot =  np.ma.masked_equal(var, var.missing_value)
 
     lons = data.variables['lon'][...]
     lats = data.variables['lat'][...]
@@ -81,12 +82,14 @@ def plot_nearside(var_plot, lons, lats, title, legend_label, cmap='viridis', con
     Adapted from Didier M. Roche a.k.a. dmr.
     """
 
+    crs = ccrs.PlateCarree()
+
     min_bounds = np.min(var_plot)
     max_bounds = np.max(var_plot)
     nbs_bounds = 15
     fix_bounds = np.linspace(min_bounds, max_bounds, nbs_bounds)
 
-    the_chosen_map = plt.cm.coolwarm
+    var_plot, lons = add_cyclic_point(var_plot, coord=lons)
 
     fig = plt.figure(figsize=(10,10))
 
@@ -96,12 +99,12 @@ def plot_nearside(var_plot, lons, lats, title, legend_label, cmap='viridis', con
     ax.set_global()
     fig.set_facecolor("grey")
 
-    mesh = ax.pcolormesh(lons, lats, var_plot, cmap=the_chosen_map,
-                         transform=ccrs.PlateCarree(), edgecolor="white",linewidths=0.05,
+    mesh = ax.pcolormesh(lons, lats, var_plot, cmap=cmap,
+                         transform=crs, edgecolor="white",linewidths=0.05,
                          vmin=min_bounds, vmax=max_bounds)
 
     if contours:
-        CS = ax.contour(lons, lats, var_plot, fix_bounds, transform=ccrs.PlateCarree(), colors='k', linewidths=1.0, vmin=min_bounds, vmax=max_bounds)
+        CS = ax.contour(lons, lats, var_plot, fix_bounds, transform=crs, colors='k', linewidths=1.0, vmin=min_bounds, vmax=max_bounds)
         ax.clabel(CS, fontsize=9, inline=1)
 
     plt.colorbar(mesh, orientation='horizontal', shrink=0.75, label=legend_label)
@@ -137,11 +140,13 @@ def map_data(var_map, lons, lats, title, legend_label, cmap='viridis', contours=
     nbs_bounds = 15
     fix_bounds = np.linspace(min_bounds, max_bounds, nbs_bounds)
 
+    var_map, lons = add_cyclic_point(var_map, coord=lons)
+
     fig, ax = plt.subplots(figsize=(10,5), subplot_kw=dict(projection=crs))
     ax.set_title(title)
     ax.set_global()
 
-    mesh = ax.pcolormesh(lons, lats, var_map, cmap=cmap,
+    mesh = ax.pcolormesh(lons, lats, var_map, norm=TwoSlopeNorm(0), cmap=cmap,
                          transform=crs,
                          vmin=min_bounds, vmax=max_bounds)
 
@@ -150,6 +155,8 @@ def map_data(var_map, lons, lats, title, legend_label, cmap='viridis', contours=
         ax.clabel(CS, fontsize=9, inline=1)
 
     plt.colorbar(mesh, orientation='vertical', shrink=0.61, label=legend_label)
+    ax.set_xticks(np.linspace(-180, 180, num=7), crs=crs)
+    ax.set_yticks(np.linspace(-90, 90, num=7), crs=crs)
     ax.gridlines()
     ax.coastlines()
     fig.show()
@@ -160,54 +167,10 @@ def map_data(var_map, lons, lats, title, legend_label, cmap='viridis', contours=
 
 ############################################################
 
-def smooth(x, window_len, window):
-    """Smooths the input data using a window with requested size.
-
-    This method is based on the convolution of a scaled window with the signal.
-    The signal is prepared by introducing reflected copies of the signal (with the window size) at both ends, so that transient parts are minimized at the begining and end parts of the output signal.
-
-    Keyword arguments:
-    x -- the input signal (array).
-    window_len -- the dimension of the smoothing window; should be an odd integer.
-    window -- the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
-                flat window will produce a moving average smoothing. See numpy documentation for explanation of these window types.
-
-
-    Adapted from Didier M. Roche a.k.a. dmr, based on original code from: http://stackoverflow.com/questions/5515720/python-smooth-time-series-data
-    """
-
-    if x.ndim != 1:
-        raise(ValueError, "smooth only accepts 1 dimension arrays.")
-
-    if x.size < window_len:
-        raise(ValueError, "Input vector needs to be bigger than window size.")
-
-
-    if window_len<3:
-        return x
-
-
-    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
-        raise(ValueError, "Window must be one of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'")
-
-
-    s=np.r_[2*x[0]-x[window_len:1:-1],x,2*x[-1]-x[-1:-window_len:-1]]
-    if window == 'flat': #moving average
-        w=np.ones(window_len,'d')
-    else:
-        w=eval('np.'+window+'(window_len)')
-
-    y=np.convolve(w/w.sum(),s,mode='same')
-    y2=y[window_len-1:-window_len+1]
-    return y2
-
-
-############################################################
-
 def read_book(filename):
-    """Reads data from filename into an pandas dataframe.
-    The input data is assumed to be daily (with 360 days per year)
-    Returns a numpy array of yearly averages.
+    """Reads data from filename into a pandas dataframe.
+    The input data is assumed to be daily.
+    Returns a pandas series of yearly averages.
 
     Keyword arguments:
     filename -- path to input file.
@@ -215,36 +178,34 @@ def read_book(filename):
 
     df = pd.read_csv(filename, sep='\s+', header=None, names=['year', 'day', 'var'])
 
-    annual = df.groupby('year')['var'].mean()
-
-    results = annual.to_numpy()
+    results = df.groupby('year')['var'].mean()
 
     return results
 
 
 ############################################################
 
-def plot_timeline(data, window, xlabel, ylabel, window_length=10, legend='on', out_file=False):
+def plot_timeline(data, title, ylabel, xlabel='Time (model years)', window_length=10, window='hann', legend='on', out_file=False):
     """Graphs a scatterplot of the data (assumed to be annual) with a 10-year running average.
 
     Keyword arguments:
-    lecture_data -- output of the `lecture_data` function.
-    window -- string, the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
-                flat window will produce a moving average smoothing. See numpy documentation for explanation of these window types.
-    xlabel -- label for the horizontal (x) axis.
+    data -- annual data (suggested: output of the `read_book` function).
+    window -- string, the type of window from the list: https://docs.scipy.org/doc/scipy/reference/signal.windows.html (default 'hann').
     ylabel -- label for the vertical (y) axis.
+    xlabel -- label for the horizontal (x) axis (default 'Time (model years)').
     window_length -- the dimension of the smoothing window (default 10).
     legend -- includes a legend with the default labels "Yearly data" and "x-year running average" (default 'on'). To disable the legend, set to 'off'.
     out_file -- optional, filepath to which to save the output graph (default False).
     """
 
-    data_smooth = smooth(data, window_len=window_length, window=window)[window_length-1: -window_length+1]
-    time = np.arange(0, len(data))
-    time_smooth = time[window_length-1: -window_length+1]
+    df = pd.DataFrame(data)
+    var = df.columns[0]
+    df['rolling'] = df[var].rolling(10, win_type='hann', center=True).mean()
 
     fig, ax = plt.subplots()
-    ax.plot(time, data, '.', color='black', markersize=1, label="Yearly data")
-    ax.plot(time_smooth, data_smooth, '-', color='indianred', linewidth=2, label="{}-year running average".format(window_length))
+    ax.set_title(title)
+    ax.plot(df.index, df[var], '.', color='black', markersize=1, label="Yearly data")
+    ax.plot(df.index[10:], df['rolling'][10:], '-', color='indianred', linewidth=2, label="{}-year running average".format(window_length))
 
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel)
